52b266e146c66163bd27b05119139f8ebcf13bc3
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-06-29 15:22:08 +0100

    move charge refunds to its own manifest file
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/Manifests/2014-06-17/Refunds.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
+ *
+ * @package    Stripe
+ * @version    1.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+return [
+
+	'all' => [
+
+		'httpMethod'     => 'GET',
+		'uri'            => '/v1/charges/{id}/refunds',
+		'summary'        => 'Retrieves a list of all the refunds of a charge.',
+		'responseClass'  => 'Cartalyst\Stripe\Api\Response',
+		'errorResponses' => $errors,
+		'parameters'     => [
+
+			'id' => [
+				'description' => 'The charge unique identifier.',
+				'location'    => 'uri',
+				'type'        => 'string',
+				'required'    => true,
+			],
+
+			'ending_before' => [
+				'description' => 'A cursor to be used in pagination.',
+				'location'    => 'query',
+				'type'        => 'string',
+				'required'    => false,
+			],
+
+			'limit' => [
+				'description' => 'A limit on the number of objects to be returned. Limit can range between 1 and 100 items.',
+				'location'    => 'query',
+				'type'        => 'integer',
+				'min'         => 1,
+				'max'         => 100,
+				'required'    => false,
+			],
+
+			'starting_after' => [
+				'description' => 'A cursor to be used in pagination.',
+				'location'    => 'query',
+				'type'        => 'string',
+				'required'    => false,
+			],
+
+		],
+
+	],
+
+	'find' => [
+
+		'httpMethod'     => 'GET',
+		'uri'            => '/v1/charges/{charge}/refunds/{id}',
+		'summary'        => 'Retrieves the given refund.',
+		'responseClass'  => 'Cartalyst\Stripe\Api\Response',
+		'errorResponses' => $errors,
+		'parameters'     => [
+
+			'charge' => [
+				'description' => 'The charge unique identifier.',
+				'location'    => 'uri',
+				'type'        => 'string',
+				'required'    => true,
+			],
+
+			'id' => [
+				'description' => 'The refund unique identifier.',
+				'location'    => 'uri',
+				'type'        => 'string',
+				'required'    => true,
+			],
+
+		],
+
+	],
+
+	'update' => [
+
+		'httpMethod'     => 'POST',
+		'uri'            => '/v1/charges/{charge}/refunds/{id}',
+		'summary'        => 'Updates the given refund.',
+		'responseClass'  => 'Cartalyst\Stripe\Api\Response',
+		'errorResponses' => $errors,
+		'parameters'     => [
+
+			'charge' => [
+				'description' => 'The charge unique identifier.',
+				'location'    => 'uri',
+				'type'        => 'string',
+				'required'    => true,
+			],
+
+			'id' => [
+				'description' => 'The refund unique identifier.',
+				'location'    => 'uri',
+				'type'        => 'string',
+				'required'    => true,
+			],
+
+			'metadata' => [
+				'description' => 'A set of key/value pairs that you can attach to a charge object.',
+				'location'    => 'query',
+				'type'        => 'array',
+				'required'    => false,
+			],
+
+		],
+
+	],
+
+];
