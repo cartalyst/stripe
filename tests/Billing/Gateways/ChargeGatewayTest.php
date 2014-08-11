e4e99f7974eeb87d7a8f81c1d4ca62fed5aa7e94
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-08-11 21:41:40 +0100

    init gateways tests
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ tests/Billing/Gateways/ChargeGatewayTest.php
@@ -0,0 +1,41 @@
+<?php namespace Cartalyst\Stripe\Tests\Billing\Gateways;
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Cartalyst PSL License.
+ *
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
+ *
+ * @package    Stripe
+ * @version    1.0.0
+ * @author     Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
+
+class ChargeGatewayTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Close mockery.
+	 *
+	 * @return void
+	 */
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/** @test */
+	public function one()
+	{
+
+	}
+
+}
