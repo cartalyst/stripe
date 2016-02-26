b0d7540c463fdec1484a0a0c79aedb0b40737cab
Bruno Gaspar <brunofgaspar1@gmail.com>
2016-02-26 18:17:39 +0000

    Add country specs api endpoints.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/CountrySpecs.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    1.0.8
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2015, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class CountrySpecs extends Api
+{
+    /**
+     * Retrieves an existing country spec.
+     *
+     * @param  string  $country
+     * @return array
+     */
+    public function find($country)
+    {
+        return $this->_get("country_specs/{$country}");
+    }
+
+    /**
+     * Returns a list of all the connected country specs.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    public function all(array $parameters = [])
+    {
+        return $this->_get('country_specs', $parameters);
+    }
+}
