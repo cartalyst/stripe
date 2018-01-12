90a2ec4154d3bf661f1a47d6e66ef6b9546b66a5
Bruno Gaspar <brunofgaspar1@gmail.com>
2018-01-12 17:20:03 +0000

    chore: Add support for Ephemeral Keys.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/EphemeralKey.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class EphemeralKey extends Api
+{
+    /**
+     * Creates a new Ephemeral Key.
+     *
+     * @param  string  $customer
+     * @return array
+     */
+    public function create(string $customer)
+    {
+        return $this->_post('ephemeral_keys', compact('customer'));
+    }
+
+    /**
+     * Deletes the given Ephemeral Key.
+     *
+     * @param  string  $ephemeralKey
+     * @return array
+     */
+    public function delete(string $ephemeralKey)
+    {
+        return $this->_delete("ephemeral_keys/{$ephemeralKey}");
+    }
+}
---
+++ tests/Api/EphemeralKeyTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Tests\Api;
+
+use Cartalyst\Stripe\Tests\FunctionalTestCase;
+
+class EphemeralKey extends FunctionalTestCase
+{
+    /** @test */
+    public function it_can_create_a_new_ephemeral_key()
+    {
+        $customer = $this->createCustomer();
+
+        $ephemeralKey = $this->stripe->ephemeralKey()->create($customer['id']);
+
+        $this->assertArrayHasKey('secret', $ephemeralKey);
+        $this->assertSame($customer['id'], $ephemeralKey['associated_objects'][0]['id']);
+    }
+
+    /** @test */
+    public function it_can_delete_an_ephemeral_key()
+    {
+        $customer = $this->createCustomer();
+
+        $ephemeralKey = $this->stripe->ephemeralKey()->create($customer['id']);
+
+        $ephemeralKey = $this->stripe->ephemeralKey()->delete($ephemeralKey['id']);
+
+        $this->assertArrayNotHasKey('secret', $ephemeralKey);
+        $this->assertSame($customer['id'], $ephemeralKey['associated_objects'][0]['id']);
+    }
+}
