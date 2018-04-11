02b629f4e52f92904dc5967ae6166a675e1a6e85
Bruno Gaspar <brunofgaspar1@gmail.com>
2018-04-11 04:05:00 +0100

    chore: Add UsageRecords API
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/UsageRecords.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.3
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class UsageRecords extends Api
+{
+    /**
+     * Create a usage record.
+     *
+     * @param  string  $itemId
+     * @param  array  $parameters
+     * @return void
+     */
+    public function create($itemId, array $parameters)
+    {
+        return $this->_post("subscription_items/{$itemId}/usage_records", $parameters);
+    }
+}
---
+++ tests/Api/UsageRecordsTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.3
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Tests\Api;
+
+use Cartalyst\Stripe\Tests\FunctionalTestCase;
+
+class UsageRecordsTest extends FunctionalTestCase
+{
+    /** @test */
+    public function it_can_create_a_new_subscription_item_usage_record()
+    {
+        $plan = $this->createPlan([
+            'usage_type' => 'metered',
+        ]);
+
+        $customer = $this->createCustomer();
+
+        $subscription = $this->createSubscription($customer['id']);
+
+        $subscriptionItem = $this->createSubscriptionItem($subscription, $plan);
+
+        $usageRecord = $this->stripe->usageRecords()->create($subscriptionItem['id'], [
+            'quantity'  => 10,
+            'timestamp' => strtotime('+3days', $subscription['current_period_start']),
+            'action'    => 'set'
+        ]);
+
+        $this->assertSame($subscriptionItem['id'], $usageRecord['subscription_item']);
+    }
+}
