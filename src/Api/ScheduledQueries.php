6be5753d0a51a89062cfe66ae034f4f4409297de
Bruno Gaspar <brunofgaspar1@gmail.com>
2018-04-11 04:04:48 +0100

    chore: Add Sigma Scheduled Queries API
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/ScheduledQueries.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.3
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class ScheduledQueries extends Api
+{
+    /**
+     * Retrieves an existing scheduled query.
+     *
+     * @param  string  $scheduleQueryRun
+     * @return array
+     */
+    public function find($scheduleQueryRun)
+    {
+        return $this->_get("sigma/scheduled_query_runs/{$scheduleQueryRun}");
+    }
+
+    /**
+     * Returns a list of all the scheduled queries.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    public function all(array $parameters = [])
+    {
+        return $this->_get('sigma/scheduled_query_runs', $parameters);
+    }
+}
