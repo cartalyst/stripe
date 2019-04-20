dd71f01e4a6b5609e5575ef1833151c5b0e25262
Bruno Gaspar <brunofgaspar1@gmail.com>
2019-04-20 20:52:52 +0100

    Add SubscriptionSchedules API
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/SubscriptionSchedules.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.4
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class SubscriptionSchedules extends Api
+{
+    /**
+     * Creates a new subscription schedule.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    public function create(array $parameters = [])
+    {
+        return $this->_post('subscription_schedules', $parameters);
+    }
+
+    /**
+     * Retrieves an existing subscription schedule.
+     *
+     * @param  string  $itemId
+     * @return array
+     */
+    public function find($itemId)
+    {
+        return $this->_get("subscription_schedules/{$itemId}");
+    }
+
+    /**
+     * Updates an existing subscription schedule.
+     *
+     * @param  string  $itemId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function update($itemId, array $parameters = [])
+    {
+        return $this->_post("subscription_schedules/{$itemId}", $parameters);
+    }
+
+    /**
+     * Cancels an existing subscription schedule.
+     *
+     * @param  string  $itemId
+     * @param  string  $parameters
+     * @return array
+     */
+    public function cancel($itemId, array $parameters = [])
+    {
+        return $this->_post("subscription_schedules/{$itemId}/cancel", $parameters);
+    }
+
+    /**
+     * Releases an existing subscription schedule.
+     *
+     * @param  string  $itemId
+     * @param  string  $parameters
+     * @return array
+     */
+    public function release($itemId, array $parameters = [])
+    {
+        return $this->_post("subscription_schedules/{$itemId}/release", $parameters);
+    }
+
+    /**
+     * Lists all subscription schedules.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    public function all(array $parameters = [])
+    {
+        return $this->_get('subscription_schedules', $parameters);
+    }
+}
---
+++ tests/Api/SubscriptionSchedulesTest.php
@@ -0,0 +1,232 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.4
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Tests\Api;
+
+use Cartalyst\Stripe\Tests\FunctionalTestCase;
+
+class SubscriptionSchedulesTest extends FunctionalTestCase
+{
+    /** @test */
+    public function it_can_create_a_new_subscription_schedule()
+    {
+        $customer = $this->createCustomer();
+
+        $plan = $this->createPlan();
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->create([
+            'customer' => $customer['id'],
+            'start_date' => time(),
+            'phases' => [
+                [
+                    'plans' => [
+                        ['plan' => $plan['id'], 'quantity' => 1],
+                    ],
+                    'iterations' => 12,
+                ],
+            ],
+            'renewal_behavior' => 'none',
+        ]);
+
+        $this->assertSame($customer['id'], $subscriptionSchedule['customer']);
+        $this->assertSame('active', $subscriptionSchedule['status']);
+    }
+
+    /** @test */
+    public function it_can_find_an_existing_subscription_schedule()
+    {
+        $customer = $this->createCustomer();
+
+        $plan = $this->createPlan();
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->create([
+            'customer' => $customer['id'],
+            'start_date' => time(),
+            'phases' => [
+                [
+                    'plans' => [
+                        ['plan' => $plan['id'], 'quantity' => 1],
+                    ],
+                    'iterations' => 12,
+                ],
+            ],
+            'renewal_behavior' => 'none',
+        ]);
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->find($subscriptionSchedule['id']);
+
+        $this->assertSame($customer['id'], $subscriptionSchedule['customer']);
+        $this->assertSame('active', $subscriptionSchedule['status']);
+    }
+
+    /**
+     * @test
+     * @expectedException \Cartalyst\Stripe\Exception\NotFoundException
+     */
+    public function it_will_throw_an_exception_when_searching_for_a_non_existing_subscription_schedule()
+    {
+        $this->stripe->subscriptionSchedules()->find(time().rand());
+    }
+
+    /** @test */
+    public function it_can_update_an_existing_subscription_schedule()
+    {
+        $customer = $this->createCustomer();
+
+        $plan = $this->createPlan();
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->create([
+            'customer' => $customer['id'],
+            'start_date' => time(),
+            'phases' => [
+                [
+                    'plans' => [
+                        ['plan' => $plan['id'], 'quantity' => 1],
+                    ],
+                    'iterations' => 12,
+                ],
+            ],
+            'renewal_behavior' => 'none',
+        ]);
+
+        $this->assertSame([], $subscriptionSchedule['metadata']);
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->update($subscriptionSchedule['id'], [
+            'metadata' => ['foo' => 'bar'],
+        ]);
+
+        $this->assertSame(['foo' => 'bar'], $subscriptionSchedule['metadata']);
+    }
+
+    /** @test */
+    public function it_can_cancel_an_existing_subscription_schedule()
+    {
+        $customer = $this->createCustomer();
+
+        $plan = $this->createPlan();
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->create([
+            'customer' => $customer['id'],
+            'start_date' => time(),
+            'phases' => [
+                [
+                    'plans' => [
+                        ['plan' => $plan['id'], 'quantity' => 1],
+                    ],
+                    'iterations' => 12,
+                ],
+            ],
+            'renewal_behavior' => 'none',
+        ]);
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->cancel($subscriptionSchedule['id']);
+
+        $this->assertSame($customer['id'], $subscriptionSchedule['customer']);
+        $this->assertSame('canceled', $subscriptionSchedule['status']);
+        $this->assertNotNull($subscriptionSchedule['canceled_at']);
+    }
+
+    /** @test */
+    public function it_can_release_an_existing_subscription_schedule()
+    {
+        $customer = $this->createCustomer();
+
+        $plan = $this->createPlan();
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->create([
+            'customer' => $customer['id'],
+            'start_date' => time(),
+            'phases' => [
+                [
+                    'plans' => [
+                        ['plan' => $plan['id'], 'quantity' => 1],
+                    ],
+                    'iterations' => 12,
+                ],
+            ],
+            'renewal_behavior' => 'none',
+        ]);
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->release($subscriptionSchedule['id']);
+
+        $this->assertSame($customer['id'], $subscriptionSchedule['customer']);
+        $this->assertSame('released', $subscriptionSchedule['status']);
+        $this->assertNotNull($subscriptionSchedule['released_at']);
+    }
+
+    /** @test */
+    public function it_can_retrieve_all_subscription_schedules()
+    {
+        $customer = $this->createCustomer();
+
+        $plan = $this->createPlan();
+
+        $subscriptionSchedule = $this->stripe->subscriptionSchedules()->create([
+            'customer' => $customer['id'],
+            'start_date' => time(),
+            'phases' => [
+                [
+                    'plans' => [
+                        ['plan' => $plan['id'], 'quantity' => 1],
+                    ],
+                    'iterations' => 12,
+                ],
+            ],
+            'renewal_behavior' => 'none',
+        ]);
+
+        $subscriptionSchedules = $this->stripe->subscriptionSchedules()->all([
+            'customer' => $customer['id'],
+        ]);
+
+        $this->assertNotEmpty($subscriptionSchedules['data']);
+        $this->assertInternalType('array', $subscriptionSchedules['data']);
+    }
+
+    /** @test */
+    public function it_can_iterate_all_subscription_schedules()
+    {
+        $customer = $this->createCustomer();
+
+        $plan = $this->createPlan();
+
+        for ($i=0; $i < 5; $i++) {
+            $this->stripe->subscriptionSchedules()->create([
+                'customer' => $customer['id'],
+                'start_date' => time(),
+                'phases' => [
+                    [
+                        'plans' => [
+                            ['plan' => $plan['id'], 'quantity' => 1],
+                        ],
+                        'iterations' => 12,
+                    ],
+                ],
+                'renewal_behavior' => 'none',
+            ]);
+        }
+
+        $subscriptionSchedules = $this->stripe->subscriptionSchedulesIterator([
+            'customer' => $customer['id'],
+        ]);
+
+        $this->assertNotEmpty($subscriptionSchedules);
+    }
+}
