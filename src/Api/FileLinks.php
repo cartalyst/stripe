b28d1ae795bfeb074b9cbc09b99b04b94310e5e1
Bruno Gaspar <brunofgaspar1@gmail.com>
2019-04-20 12:08:34 +0100

    Add FileLinks API
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/FileLinks.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.4
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class FileLinks extends Api
+{
+    /**
+     * Creates a new file link.
+     *
+     * @param  string  $fileId
+     * @param  array  $attributes
+     * @return array
+     */
+    public function create($fileId, array $attributes = [])
+    {
+        $attributes = array_merge($attributes, [
+            'file' => $fileId,
+        ]);
+
+        return $this->_post("file_links", $attributes);
+    }
+
+    /**
+     * Retrieves an existing file link.
+     *
+     * @param  string  $fileLinkId
+     * @return array
+     */
+    public function find($fileLinkId)
+    {
+        return $this->_get("file_links/{$fileLinkId}");
+    }
+
+    /**
+     * Updates an existing file link.
+     *
+     * @param  string  $fileLinkId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function update($fileLinkId, array $parameters = [])
+    {
+        return $this->_post("file_links/{$fileLinkId}", $parameters);
+    }
+
+    /**
+     * Lists all file links.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    public function all(array $parameters = [])
+    {
+        return $this->_get('file_links', $parameters);
+    }
+}
---
+++ tests/Api/FileLinksTest.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.4
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Tests\Api;
+
+use Cartalyst\Stripe\Tests\FunctionalTestCase;
+
+class FileLinksTest extends FunctionalTestCase
+{
+    /** @test */
+    public function it_can_create_a_file_link()
+    {
+        $filePath = realpath(__DIR__.'/../files/verify-account.jpg');
+
+        $upload = $this->stripe->files()->create($filePath, 'dispute_evidence');
+
+        $fileLink = $this->stripe->fileLinks()->create($upload['id']);
+
+        $this->assertSame($upload['id'], $fileLink['file']);
+    }
+
+    /** @test */
+    public function it_can_retrieve_a_file_link()
+    {
+        $filePath = realpath(__DIR__.'/../files/verify-account.jpg');
+
+        $upload = $this->stripe->files()->create($filePath, 'dispute_evidence');
+
+        $fileLink = $this->stripe->fileLinks()->create($upload['id']);
+
+        $fileLink = $this->stripe->fileLinks()->find($fileLink['id']);
+
+        $this->assertSame($upload['id'], $fileLink['file']);
+    }
+
+    /** @test */
+    public function it_can_update_a_file_link()
+    {
+        $filePath = realpath(__DIR__.'/../files/verify-account.jpg');
+
+        $upload = $this->stripe->files()->create($filePath, 'dispute_evidence');
+
+        $fileLink = $this->stripe->fileLinks()->create($upload['id']);
+
+        $this->assertSame([], $fileLink['metadata']);
+
+        $fileLink = $this->stripe->fileLinks()->update($fileLink['id'], [
+            'metadata' => ['foo' => 'bar'],
+        ]);
+
+        $this->assertSame(['foo' => 'bar'], $fileLink['metadata']);
+    }
+
+    /** @test */
+    public function it_can_retrieve_all_file_links()
+    {
+        $filePath = realpath(__DIR__.'/../files/verify-account.jpg');
+
+        $upload = $this->stripe->files()->create($filePath, 'dispute_evidence');
+
+        $fileLink = $this->stripe->fileLinks()->create($upload['id']);
+        $fileLink = $this->stripe->fileLinks()->create($upload['id']);
+        $fileLink = $this->stripe->fileLinks()->create($upload['id']);
+
+        $fileLinks = $this->stripe->fileLinks()->all([
+            'file' => $upload['id'],
+        ]);
+
+        $this->assertCount(3, $fileLinks['data']);
+    }
+
+    /** @test */
+    public function it_can_iterate_all_file_links()
+    {
+        $filePath = realpath(__DIR__.'/../files/verify-account.jpg');
+
+        $upload = $this->stripe->files()->create($filePath, 'dispute_evidence');
+
+        for ($i=0; $i < 5; $i++) {
+            $this->stripe->fileLinks()->create($upload['id']);
+        }
+
+        $fileLinks = $this->stripe->fileLinksIterator([
+            'file' => $upload['id'],
+        ]);
+
+        $this->assertCount(5, $fileLinks);
+    }
+}
