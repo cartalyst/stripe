5b12e54a73b91a52416529809cab46a038e46b45
Bruno Gaspar <brunofgaspar1@gmail.com>
2019-04-20 12:10:14 +0100

    Add CreditNotes API
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/CreditNotes.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.4
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class CreditNotes extends Api
+{
+    /**
+     * Creates a new credit note.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    public function create(array $parameters = [])
+    {
+        return $this->_post('credit_notes', $parameters);
+    }
+
+    /**
+     * Retrieves an existing credit note.
+     *
+     * @param  string  $creditNoteId
+     * @return array
+     */
+    public function find($creditNoteId)
+    {
+        return $this->_get("credit_notes/{$creditNoteId}");
+    }
+
+    /**
+     * Updates an existing credit notes.
+     *
+     * @param  string  $creditNoteId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function update($creditNoteId, array $parameters = [])
+    {
+        return $this->_post("credit_notes/{$creditNoteId}", $parameters);
+    }
+
+    /**
+     * Void the given credit note.
+     *
+     * @param  string  $creditNoteId
+     * @return array
+     */
+    public function void($creditNoteId)
+    {
+        return $this->_post("credit_notes/{$creditNoteId}/void");
+    }
+
+    /**
+     * Lists all credit notes.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    public function all(array $parameters = [])
+    {
+        return $this->_get('credit_notes', $parameters);
+    }
+}
---
+++ tests/Api/CreditNotesTest.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.1.4
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Tests\Api;
+
+use Cartalyst\Stripe\Tests\FunctionalTestCase;
+
+class CreditNotesTest extends FunctionalTestCase
+{
+    /** @test */
+    public function it_can_create_a_new_credit_note()
+    {
+        $customer = $this->createCustomer();
+
+        $this->createInvoiceItem($customer['id'], [
+            'amount' => 1000
+        ]);
+
+        $invoice = $this->createInvoice($customer['id']);
+
+        $invoice = $this->stripe->invoices()->finalize($invoice['id']);
+
+        $creditNote = $this->stripe->creditNotes()->create([
+            'amount' => 500,
+            'invoice' => $invoice['id'],
+        ]);
+
+        $this->assertSame(50000, $creditNote['amount']);
+        $this->assertSame('issued', $creditNote['status']);
+    }
+
+    /** @test */
+    public function it_can_find_an_existing_credit_note()
+    {
+        $customer = $this->createCustomer();
+
+        $this->createInvoiceItem($customer['id'], [
+            'amount' => 1000
+        ]);
+
+        $invoice = $this->createInvoice($customer['id']);
+
+        $invoice = $this->stripe->invoices()->finalize($invoice['id']);
+
+        $creditNote = $this->stripe->creditNotes()->create([
+            'amount' => 500,
+            'invoice' => $invoice['id'],
+        ]);
+
+        $creditNote = $this->stripe->creditNotes()->find($creditNote['id']);
+
+        $this->assertSame(50000, $creditNote['amount']);
+        $this->assertSame('issued', $creditNote['status']);
+    }
+
+    /**
+     * @test
+     * @expectedException \Cartalyst\Stripe\Exception\NotFoundException
+     */
+    public function it_will_throw_an_exception_when_searching_for_a_non_existing_credit_note()
+    {
+        $this->stripe->creditNotes()->find(time().rand());
+    }
+
+    /** @test */
+    public function it_can_update_an_existing_credit_note()
+    {
+        $customer = $this->createCustomer();
+
+        $this->createInvoiceItem($customer['id'], [
+            'amount' => 1000
+        ]);
+
+        $invoice = $this->createInvoice($customer['id']);
+
+        $invoice = $this->stripe->invoices()->finalize($invoice['id']);
+
+        $creditNote = $this->stripe->creditNotes()->create([
+            'amount' => 500,
+            'invoice' => $invoice['id'],
+        ]);
+
+        $creditNote = $this->stripe->creditNotes()->update($creditNote['id'], [
+            'metadata' => ['foo' => 'bar'],
+        ]);
+
+        $this->assertSame(['foo' => 'bar'], $creditNote['metadata']);
+    }
+
+    /** @test */
+    public function it_can_void_an_existing_credit_note()
+    {
+        $customer = $this->createCustomer();
+
+        $this->createInvoiceItem($customer['id'], [
+            'amount' => 1000
+        ]);
+
+        $invoice = $this->createInvoice($customer['id']);
+
+        $invoice = $this->stripe->invoices()->finalize($invoice['id']);
+
+        $creditNote = $this->stripe->creditNotes()->create([
+            'amount' => 500,
+            'invoice' => $invoice['id'],
+        ]);
+
+        $creditNote = $this->stripe->creditNotes()->void($creditNote['id']);
+
+        $this->assertSame(50000, $creditNote['amount']);
+        $this->assertSame('void', $creditNote['status']);
+    }
+
+    /** @test */
+    public function it_can_iterate_all_credit_notes()
+    {
+        $customer = $this->createCustomer();
+
+        $this->createInvoiceItem($customer['id'], [
+            'amount' => 1000
+        ]);
+
+        $invoice = $this->createInvoice($customer['id']);
+
+        $invoice = $this->stripe->invoices()->finalize($invoice['id']);
+
+        $this->stripe->creditNotes()->create([
+            'amount' => 500,
+            'invoice' => $invoice['id'],
+        ]);
+
+        $this->stripe->creditNotes()->create([
+            'amount' => 100,
+            'invoice' => $invoice['id'],
+        ]);
+
+        $creditNotes = $this->stripe->creditNotesIterator([
+            'invoice' => $invoice['id'],
+        ]);
+
+        $this->assertCount(2, $creditNotes);
+    }
+}
