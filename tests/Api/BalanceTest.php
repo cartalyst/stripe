c89ec05ce4fb3743bfc62cd13c5603c2c5d21336
Bruno Gaspar <brunofgaspar1@gmail.com>
2019-04-21 15:43:09 +0100

    chore: Update copyright year
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- tests/Api/BalanceTest.php
+++ tests/Api/BalanceTest.php
@@ -14,7 +14,7 @@
  * @version    2.1.4
  * @author     Cartalyst LLC
  * @license    BSD License (3-clause)
- * @copyright  (c) 2011-2018, Cartalyst LLC
+ * @copyright  (c) 2011-2019, Cartalyst LLC
  * @link       http://cartalyst.com
  */
 
