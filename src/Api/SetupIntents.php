9a5cddee25293c979963a2301d7977801f68ec71
Bruno Gaspar <brunofgaspar1@gmail.com>
2019-07-03 18:44:51 +0100

    chore: Add SetupIntents API
    Closes: #135
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/SetupIntents.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.2.5
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2019, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class SetupIntents extends Api
+{
+    /**
+     * Creates a new setup intent.
+     *
+     * @param  array  $parameters
+     * @return array
+     */
+    public function create(array $parameters = [])
+    {
+        return $this->_post('setup_intents', $parameters);
+    }
+
+    /**
+     * Retrieves an existing setup intent.
+     *
+     * @param  string  $setupIntentId
+     * @return array
+     */
+    public function find($setupIntentId)
+    {
+        return $this->_get("setup_intents/{$setupIntentId}");
+    }
+
+    /**
+     * Updates an existing setup intents.
+     *
+     * @param  string  $setupIntentId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function update($setupIntentId, array $parameters = [])
+    {
+        return $this->_post("setup_intents/{$setupIntentId}", $parameters);
+    }
+
+    /**
+     * Confirm the given setup intent.
+     *
+     * @param  string  $setupIntentId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function confirm($setupIntentId, array $parameters = [])
+    {
+        return $this->_post("setup_intents/{$setupIntentId}/confirm", $parameters);
+    }
+
+    /**
+     * Cancels the given setup intent.
+     *
+     * @param  string  $setupIntentId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function cancel($setupIntentId, array $parameters = [])
+    {
+        return $this->_post("setup_intents/{$setupIntentId}/cancel", $parameters);
+    }
+}
---
+++ tests/Api/SetupIntentsTest.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.2.5
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2019, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Tests\Api;
+
+use Cartalyst\Stripe\Tests\FunctionalTestCase;
+
+class SetupIntentsTest extends FunctionalTestCase
+{
+    /** @test */
+    public function it_can_create_a_new_setup_intent()
+    {
+        $customer = $this->createCustomer();
+
+        $setupIntent = $this->stripe->setupIntents()->create([
+            'customer'             => $customer['id'],
+            'payment_method_types' => ['card'],
+        ]);
+
+        $this->assertSame($customer['id'], $setupIntent['customer']);
+        $this->assertSame(['card'], $setupIntent['payment_method_types']);
+    }
+
+    /** @test */
+    public function it_can_create_a_new_setup_intent_with_a_card_upfront()
+    {
+        $customer = $this->createCustomer();
+
+        $card = $this->createCardThroughArray($customer['id']);
+
+        $setupIntent = $this->stripe->setupIntents()->create([
+            'customer'             => $customer['id'],
+            'payment_method'       => $card['id'],
+            'payment_method_types' => ['card'],
+        ]);
+
+        $this->assertSame($customer['id'], $setupIntent['customer']);
+        $this->assertSame($card['id'], $setupIntent['payment_method']);
+        $this->assertSame(['card'], $setupIntent['payment_method_types']);
+    }
+
+    /** @test */
+    public function it_can_find_an_existing_setup_intent()
+    {
+        $customer = $this->createCustomer();
+
+        $setupIntent = $this->stripe->setupIntents()->create([
+            'customer'             => $customer['id'],
+            'payment_method_types' => ['card'],
+        ]);
+
+        $setupIntent = $this->stripe->setupIntents()->find($setupIntent['id']);
+
+        $this->assertSame($customer['id'], $setupIntent['customer']);
+        $this->assertSame(['card'], $setupIntent['payment_method_types']);
+    }
+
+    /**
+     * @test
+     * @expectedException \Cartalyst\Stripe\Exception\NotFoundException
+     */
+    public function it_will_throw_an_exception_when_searching_for_a_non_existing_setup_intent()
+    {
+        $this->stripe->setupIntents()->find(time().rand());
+    }
+
+    /** @test */
+    public function it_can_update_an_existing_setup_intent()
+    {
+        $customer = $this->createCustomer();
+
+        $setupIntent = $this->stripe->setupIntents()->create([
+            'customer'             => $customer['id'],
+            'payment_method_types' => ['card'],
+        ]);
+
+        $setupIntent = $this->stripe->setupIntents()->update($setupIntent['id'], [
+            'description' => 'Some Description',
+        ]);
+
+        $this->assertSame($customer['id'], $setupIntent['customer']);
+        $this->assertSame(['card'], $setupIntent['payment_method_types']);
+        $this->assertSame('Some Description', $setupIntent['description']);
+    }
+
+    /** @test */
+    public function it_can_confirm_an_existing_setup_intent()
+    {
+        $customer = $this->createCustomer();
+
+        $card = $this->createCardThroughArray($customer['id']);
+
+        $setupIntent = $this->stripe->setupIntents()->create([
+            'customer'             => $customer['id'],
+            'payment_method'       => $card['id'],
+            'payment_method_types' => ['card'],
+        ]);
+
+        $setupIntent = $this->stripe->setupIntents()->confirm($setupIntent['id']);
+
+        $this->assertSame('succeeded', $setupIntent['status']);
+    }
+
+    /** @test */
+    public function it_can_cancel_an_existing_setup_intent()
+    {
+        $customer = $this->createCustomer();
+
+        $card = $this->createCardThroughArray($customer['id']);
+
+        $setupIntent = $this->stripe->setupIntents()->create([
+            'customer'             => $customer['id'],
+            'payment_method'       => $card['id'],
+            'payment_method_types' => ['card'],
+        ]);
+
+        $setupIntent = $this->stripe->setupIntents()->cancel($setupIntent['id']);
+
+        $this->assertSame('canceled', $setupIntent['status']);
+    }
+}
