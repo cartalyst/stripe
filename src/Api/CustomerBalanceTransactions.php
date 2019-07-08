70d63d7582ebbb1f39572853e7e3c8efdc7d437e
Bruno Gaspar <brunofgaspar1@gmail.com>
2019-07-08 22:03:25 +0100

    chore: Add CustomerBalanceTransaction API
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Api/CustomerBalanceTransactions.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.2.8
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2019, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class CustomerBalanceTransactions extends Api
+{
+    /**
+     * Creates a new customer balance transaction.
+     *
+     * @param  string  $customerId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function create($customerId, array $parameters = [])
+    {
+        return $this->_post("customers/{$customerId}/balance_transactions", $parameters);
+    }
+
+    /**
+     * Retrieves an existing customer balance transaction.
+     *
+     * @param  string  $customerId
+     * @param  string  $transactionId
+     * @return array
+     */
+    public function find($customerId, $transactionId)
+    {
+        return $this->_get("customers/{$customerId}/balance_transactions/{$transactionId}");
+    }
+
+    /**
+     * Updates an existing customer balance transaction.
+     *
+     * @param  string  $customerId
+     * @param  string  $transactionId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function update($customerId, $transactionId, array $parameters = [])
+    {
+        return $this->_post("customers/{$customerId}/balance_transactions/{$transactionId}", $parameters);
+    }
+
+    /**
+     * Lists all the balance transactions for the given customer.
+     *
+     * @param  string  $customerId
+     * @param  array  $parameters
+     * @return array
+     */
+    public function all($customerId, array $parameters = [])
+    {
+        return $this->_get("customers/{$customerId}/balance_transactions", $parameters);
+    }
+}
---
+++ tests/Api/CustomerBalanceTransactionsTest.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.2.8
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2019, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Tests\Api;
+
+use Cartalyst\Stripe\Tests\FunctionalTestCase;
+
+class CustomerBalanceTransactionsTest extends FunctionalTestCase
+{
+    /** @test */
+    public function it_can_create_a_new_customer_balance_transaction()
+    {
+        $customer = $this->createCustomer();
+
+        $balanceTransaction = $this->stripe->customerBalanceTransactions()->create($customer['id'], [
+            'amount'  => '-25.50',
+            'currency' => 'USD',
+        ]);
+
+        $this->assertSame($customer['id'], $balanceTransaction['customer']);
+        $this->assertSame(-2550, $balanceTransaction['amount']);
+    }
+
+    /** @test */
+    public function it_can_find_an_existing_customer_balance_transaction()
+    {
+        $customer = $this->createCustomer();
+
+        $balanceTransaction = $this->stripe->customerBalanceTransactions()->create($customer['id'], [
+            'amount'  => '-25.50',
+            'currency' => 'USD',
+        ]);
+
+        $balanceTransaction = $this->stripe->customerBalanceTransactions()->find($customer['id'], $balanceTransaction['id']);
+
+        $this->assertSame($customer['id'], $balanceTransaction['customer']);
+        $this->assertSame(-2550, $balanceTransaction['amount']);
+    }
+
+    /**
+     * @test
+     * @expectedException \Cartalyst\Stripe\Exception\NotFoundException
+     */
+    public function it_will_throw_an_exception_when_searching_for_a_non_existing_customer_balance_transaction()
+    {
+        $customer = $this->createCustomer();
+
+        $this->stripe->customerBalanceTransactions()->find($customer['id'], time());
+    }
+
+    /** @test */
+    public function it_can_update_an_existing_customer_balance_transaction()
+    {
+        $customer = $this->createCustomer();
+
+        $balanceTransaction = $this->stripe->customerBalanceTransactions()->create($customer['id'], [
+            'amount'  => '-25.50',
+            'currency' => 'USD',
+        ]);
+
+        $balanceTransaction = $this->stripe->customerBalanceTransactions()->update($customer['id'], $balanceTransaction['id'], [
+            'metadata' => [ 'name' => 'John Doe' ],
+        ]);
+
+        $this->assertSame($customer['id'], $balanceTransaction['customer']);
+        $this->assertSame('John Doe', $balanceTransaction['metadata']['name']);
+    }
+
+    /** @test */
+    public function it_can_retrieve_all_balance_transactions_of_a_customer()
+    {
+        $customer = $this->createCustomer();
+
+        $this->stripe->customerBalanceTransactions()->create($customer['id'], [
+            'amount'  => '25.50',
+            'currency' => 'USD',
+        ]);
+
+        $balanceTransactions = $this->stripe->customerBalanceTransactions()->all($customer['id']);
+
+        $this->assertCount(1, $balanceTransactions['data']);
+    }
+
+    /** @test */
+    public function it_can_iterate_all_balance_transactions_of_a_customer()
+    {
+        $customer = $this->createCustomer();
+
+        for ($i=0; $i < 5; $i++) {
+            $this->stripe->customerBalanceTransactions()->create($customer['id'], [
+                'amount'  => '25.50',
+                'currency' => 'USD',
+            ]);
+        }
+
+        $balanceTransactions = $this->stripe->customerBalanceTransactionsIterator($customer['id']);
+
+        $this->assertCount(5, $balanceTransactions);
+    }
+}
