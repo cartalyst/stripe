9394411a0ddc550a4140afe779a496dde8b37d93
Bruno Gaspar <brunofgaspar1@gmail.com>
2020-01-03 18:48:24 +0000

    chore: Remove unnecessary methods from the Api class
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- src/Api/ApiInterface.php
+++ src/Api/ApiInterface.php
@@ -62,15 +62,6 @@ interface ApiInterface
     public function _get($url = null, $parameters = []);
 
     /**
-     * Send a HEAD request.
-     *
-     * @param  string  $url
-     * @param  array  $parameters
-     * @return array
-     */
-    public function _head($url = null, array $parameters = []);
-
-    /**
      * Send a DELETE request.
      *
      * @param  string  $url
@@ -80,24 +71,6 @@ interface ApiInterface
     public function _delete($url = null, array $parameters = []);
 
     /**
-     * Send a PUT request.
-     *
-     * @param  string  $url
-     * @param  array  $parameters
-     * @return array
-     */
-    public function _put($url = null, array $parameters = []);
-
-    /**
-     * Send a PATCH request.
-     *
-     * @param  string  $url
-     * @param  array  $parameters
-     * @return array
-     */
-    public function _patch($url = null, array $parameters = []);
-
-    /**
      * Send a POST request.
      *
      * @param  string  $url
@@ -105,15 +78,6 @@ interface ApiInterface
      * @return array
      */
     public function _post($url = null, array $parameters = []);
-
-    /**
-     * Send an OPTIONS request.
-     *
-     * @param  string  $url
-     * @param  array  $parameters
-     * @return array
-     */
-    public function _options($url = null, array $parameters = []);
 
     /**
      * Executes the HTTP request.
