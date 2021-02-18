ba4b180f20f1947dc3e449dc84637d83ace36906
Alexander Rieder <alexander.rieder@dvision.org>
2021-02-18 12:47:49 +0100

    added Mandates Endpoint issue https://github.com/cartalyst/stripe/issues/199

---
+++ src/Api/Mandates.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Part of the Stripe package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Stripe
+ * @version    2.4.3
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2021, Cartalyst LLC
+ * @link       https://cartalyst.com
+ */
+
+namespace Cartalyst\Stripe\Api;
+
+class Mandates extends Api
+{
+    /**
+     * Retrieves an existing mandate.
+     *
+     * @param  string  $mandateId
+     * @return array
+     */
+    public function find($mandateId)
+    {
+        return $this->_get("mandates/{$mandateId}");
+    }
+}
